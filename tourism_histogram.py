720e1360c25e89ef193511a416e7ae116c808088
raphaelseegmuller <46761917+raphaelseegmuller@users.noreply.github.com>
2021-01-09 15:49:51 +0100

    Update tourism_histogram.py

--- tourism_histogram.py
+++ tourism_histogram.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from libs.function import get_histogram_values
 from tourism_data import Department_tourism_2019_list, Paris_tourism_2019
 
+# Histogram scale (can be modified) #
 scale = 250000
+
 bins_list = [0]
 sc = 0
 while sc < Paris_tourism_2019:
     sc += scale
     bins_list += [sc]
-
-print(get_histogram_values(scale))
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -19,5 +19,7 @@ plt.ylabel("French department number")
 plt.title("Histogram of nights number per french departments (without Corsica)")
 ax.hist(Department_tourism_2019_list, bins=bins_list)
 
+# A = np.array(Department_tourism_2019_list)
+# print(np.median(A))
 
 plt.show()
